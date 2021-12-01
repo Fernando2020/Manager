aacdc89042e928d3a55af11cf5140572c4afce13
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-12-01 20:21:56 -0300

    Configurado a camada de Services

---
+++ src/Manager.Services/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Manager.Services.DTO
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserDTO()
+        {
+
+        }
+
+        public UserDTO(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
